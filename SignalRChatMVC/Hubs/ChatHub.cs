8ec70becc71306b80a68697fe73d36b5712ed649
GermanF <ferreyra.ger@gmail.com>
2013-08-28 16:13:15 -0300

    SignalR Solution Samples

---
+++ SignalRChatMVC/Hubs/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SignalRChatMVC.Hubs
+{
+    public class ChatHub: Hub
+    {
+        public void Send(string name, string msg)
+        {
+            Clients.All.addNewMessageToPage(name, msg);
+        }
+    }
+}
